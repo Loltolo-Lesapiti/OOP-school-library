9ee464bc521ac383e8fe29941d08de97fdebd519
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-11-17 01:12:55 +0300

    Create a student class

---
+++ student.rb
@@ -0,0 +1,11 @@
+require_relative 'person'
+class Student < Person
+    def initialize(classroom)
+        super(age, name)
+        @classroom=classroom
+    end
+    def play_hooky
+        return "¯\(ツ)/¯"
+    end
+end
+
