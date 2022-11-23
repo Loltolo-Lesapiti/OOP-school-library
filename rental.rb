d55a716e19a988030130c8c8485138758ad16b71
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-11-23 23:23:26 +0300

    Update rental and teacher

---
+++ rental.rb
@@ -0,0 +1,17 @@
+class Rental
+  attr_reader :book, :person
+  attr_accessor :date
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rentals << self
+
+    @person = person
+    person.rentals << self
+  end
+  def create_object
+    { book_title: @book.title, person_id: @person, date: @date }
+  end
+
+end
